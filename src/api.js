18347ba126945f03bdfe910a651074cc45927ae8
Mr Miser <mrmiser662003@gmail.com>
2026-06-15 01:50:59 +0700

    Fix API URL HTTPS

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://approval-subsection-farms-gifts.trycloudflare.com",
+  baseURL: "https://approval-subsection-farms-gifts.trycloudflare.com",
 });
 
 export default api;
